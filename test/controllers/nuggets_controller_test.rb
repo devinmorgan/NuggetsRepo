f51dfd9cffcd68e762f9d3ef22ad0c3cc13ac2ef
Devin Morgan <devinm@mit.edu>
2017-06-18 19:36:54 -0400

    Got rid of the nuggets controller

--- test/controllers/nuggets_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class NuggetsControllerTest < ActionDispatch::IntegrationTest
-  test "should get gallery" do
-    get nuggets_gallery_url
-    assert_response :success
-  end
-
-end
