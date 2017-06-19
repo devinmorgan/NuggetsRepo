652921d018a8ccfa49706a928c2c53e657ac079f
Devin Morgan <devinm@mit.edu>
2017-06-18 20:50:47 -0400

    Created the EbooksUploader

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'nuggets/gallery'
-  root 'nuggets#gallery'
+  # get 'nuggets/gallery'
+  # root 'nuggets#gallery'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
