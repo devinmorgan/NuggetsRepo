5981ef969d9182fa227869f588838f9a5023bc53
Devin Morgan <devinm@mit.edu>
2017-07-29 20:58:22 -0400

    Increased size of iframe, added prev and next section buttons

--- app/assets/javascripts/ebooks/render_ebook.js
+++ app/assets/javascripts/ebooks/render_ebook.js
@@ -1,3 +1,15 @@
 /**
  * Created by nerds on 6/25/2017.
  */
+var iframe = null;
+var iframeDocument = null;
+function onloadHandler() {
+    iframe = document.querySelector('#ebook-section');
+    iframeDocument = iframe.contentDocument || iframe.contentWindow.document;
+}
+
+function loadPreviousSection() {
+    // 1) get the current section number from #ebook-console
+    var current_section_number = document.getElementById("ebook-console").dataset.currentSection;
+    console.log("Current Section Number: " + current_section_number);
+}
